559efe100740b0713f72b5dbf582da27901e00de
NinputerWonder <ninputewonder@outlook.com>
2020-09-21 22:32:13 +0800

    add index

---
+++ src/test/java/com/wonder/AssignmentService/AssignmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonder.AssignmentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
